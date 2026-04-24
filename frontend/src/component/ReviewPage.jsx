43cbcf876835027860a8b95edd91805063d10130
TammanaSethi <tammanasethi1035@gmail.com>
2026-04-24 20:29:35 +0530

    fix case issue ReviewCard

--- frontend/src/component/ReviewPage.jsx
+++ frontend/src/component/ReviewPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import ReviewCard from "./reviewCard";
+import ReviewCard from "./ReviewCard";
 
 import {
   setReviewData,
