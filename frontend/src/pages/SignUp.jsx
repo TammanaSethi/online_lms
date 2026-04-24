f891baf32012f281a119fb96e69524f1ee7b67b4
Tammana <tammanasethi1035@gmail.com>
2026-04-24 17:33:51 +0530

    Update server URL for SignUp component

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -9,7 +9,7 @@ const SignUp = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://online-lms-xnob.onrender.com";
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
